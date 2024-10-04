9ef0947041e401da259def92fd5c980b2e31df98
yabash247 <yabashllc@gmail.com>
2024-10-03 23:26:20 -0500

    working here

--- backend/settings.py
+++ backend/settings.py
@@ -85,10 +85,24 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'projects',
+        'USER': 'root',
+        'PASSWORD': 'yabash247365',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
