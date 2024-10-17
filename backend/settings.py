6524521ef3afdeebbc8ece665a57c0a4f36fdb9f
yabash247 <yabashllc@gmail.com>
2024-10-17 04:37:45 -0500

    changes

--- backend/settings.py
+++ backend/settings.py
@@ -110,6 +110,7 @@ DATABASES = {
 
 
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -135,6 +136,7 @@ DATABASES = {
     }
 }
 
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
